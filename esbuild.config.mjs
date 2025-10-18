510196c9a4e49ce185cbd8b20d47af57c293218b
MagneticNeedle <solankivibhakar82@gmail.com>
2025-10-19 00:42:46 +0530

    feat: add esbuild support

---
+++ esbuild.config.mjs
@@ -0,0 +1,12 @@
+import * as esbuild from 'esbuild'
+
+await esbuild.build({
+    entryPoints: ['src/index.ts'],
+    bundle: true,
+    outdir: 'dist',
+    format: 'esm',
+    banner: {
+        js: '// @ts-nocheck'
+        
+    },
+})
--- src/index.ts
+++ src/index.ts
@@ -16,4 +16,3 @@ export function OngoingGlobal() {
 	init = true;
 }
 
-
