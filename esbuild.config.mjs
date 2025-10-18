9ea6824b53e1251465762d465519dd9a8189b83f
MagneticNeedle <solankivibhakar82@gmail.com>
2025-10-19 01:16:44 +0530

    feat: update banner

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,12 +1,18 @@
 import * as esbuild from 'esbuild'
+import pkg from './package.json' with { type: 'json' };
 
+const banner = `// @ts-nocheck
+/*
+${pkg.name} v${pkg.version} | (c) ${pkg.author.name} | ${pkg.repository.url}
+Created using https://github.com/battlefield-portal-community/PortalScriptingTemplateRepo
+*/`;
 await esbuild.build({
     entryPoints: ['src/index.ts'],
     bundle: true,
     outdir: 'dist',
     format: 'esm',
     banner: {
-        js: '// @ts-nocheck'
+        js: banner
         
     },
 })
